a9120ea20fb3b0e74764a82131579a9f0223eaf2
Sascha Leib <sascha.leib@kolmio.com>
2025-11-03 21:40:39 +0100

    Minor fix

--- action.php
+++ action.php
@@ -310,7 +310,7 @@ class action_plugin_botmon extends DokuWiki_Action_Plugin {
 		$raw = $this->getConf('captchaSeed') . '|' . $_SERVER['SERVER_NAME'] . '|' . $_SERVER['REMOTE_ADDR'] . '|' . $today;
 		$expected = hash('sha256', $raw);
 
-		echo '<!-- cookieVal: ' . $cookieVal . ', expected: ' . $expected . ', seed: ' . $this->getConf('captchaSeed') . ', server: ' . $_SERVER['SERVER_NAME'] . ', ip: ' . $_SERVER['REMOTE_ADDR'] . ', date: ' . $today .'\' -->' . NL;
+		//echo '<!-- cookieVal: ' . $cookieVal . ', expected: ' . $expected . ', seed: ' . $this->getConf('captchaSeed') . ', server: ' . $_SERVER['SERVER_NAME'] . ', ip: ' . $_SERVER['REMOTE_ADDR'] . ', date: ' . $today .'\' -->' . NL;
 
 		return $cookieVal == $expected;
 	}
