73f88ce3c9e6729d6417f337a35bd1c3dc6f0451
Sascha Leib <saschaleib@users.noreply.github.com>
2026-05-22 12:08:50 +0200

    Update GeoIP and Captcha settings with links

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -11,17 +11,18 @@ $lang['showday']			= 'Which data to show in the “Latest” tab:';
 
 $lang['combineNets']	= 'Combine visits from known IP-ranges into one entry:';
 
-$lang['geoiplib']			= 'Add GeoIP Information<br><small>(requires <a href="https://www.php.net/manual/en/book.geoip.php" target="_blank">GeoIP</a> PHP module)</small>';
+$lang['geoiplib']			= 'Add GeoIP Information <a href="https://leib.be/sascha/projects/dokuwiki/botmon/config/settings#add_geoip_information" target="_blank" title="More information">🛈</a>';
 	$lang['geoiplib_o_disabled']		= 'Disabled';
-	$lang['geoiplib_o_phpgeoip']		= 'Use GeoIP Module';
+	$lang['geoiplib_o_phpgeoip']		= 'Use PHP GeoIP Module';
+	$lang['geoiplib_o_cloudflare']		= 'Use Cloudflare GeoIP';
 
-$lang['useCaptcha']		= 'Enable Captcha<br><small>(Experimental, read the manual first!)</small>';
+$lang['useCaptcha']		= 'Enable Captcha <a href="https://leib.be/sascha/projects/dokuwiki/botmon/config/settings#enable_captcha" target="_blank" title="More information">🛈</a>';
 	$lang['useCaptcha_o_disabled']		= 'Disabled';
 	$lang['useCaptcha_o_loremipsum']	= 'Captcha with Lorem ipsum text';
 	$lang['useCaptcha_o_dada']			= 'Captcha with Dada placeholder text';
 
 $lang['captchaSeed']	= 'Captcha Seed<br><small>(Enter a random string, e.g. <a href="https://www.browserling.com/tools/random-hex" target="_blank">from here</a>)</small>';
 
-$lang['captchaOptions']		= 'Captcha options<br><small>(Make sure to read the <a href="https://leib.be/sascha/projects/dokuwiki/botmon/config/settings#captcha_options" target="_blank">documentation</a> first)</small>';
+$lang['captchaOptions']		= 'Captcha options <a href="https://leib.be/sascha/projects/dokuwiki/botmon/config/settings#captcha_options" target="_blank" title="More information">🛈</a>';
 	$lang['captchaOptions_langmatch']	= 'Language match bypass';
 	$lang['captchaOptions_anyval']		= 'Ignore cookie value';
